d17e752fe589abdfa56e18dc11ad2633c3a6873e
Kishan <kishansambhi@hotmail.co.uk>
2018-09-04 14:56:48 +0100

    :art: Add separte keypress event section

--- pi/detect-input.py
+++ pi/detect-input.py
@@ -1,12 +1,16 @@
 # Code originally from https://stackoverflow.com/questions/5060710/format-of-dev-input-event
 # I don't know how to handle structs, so i'm just leaving as is (i.e. in Python)
 # Would be better if in Java (like rest of project), C++/C (native code), JS (like server)
-# as it means rest of pi side has to be in python 
+# as it means rest of pi side might have to be in python 
+#
+# See https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h for codes 
+# and https://www.kernel.org/doc/Documentation/input/event-codes.txt for meanings
+
 import struct
 import time
 import sys
 
-infile_path = "/dev/input/event" + (sys.argv[1] if len(sys.argv) > 1 else "0")
+infile_path = "/dev/input/event" + (sys.argv[1] if len(sys.argv) > 1 else "0") # File for input that corresponds to the keyboard.  Should use human readable ones in /dev/input/by-id
 
 #long int, long int, unsigned short, unsigned short, unsigned int
 FORMAT = 'llHHI'
@@ -21,7 +25,11 @@ while event:
     (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
     print(type)
 
-    if type != 0 or code != 0 or value != 0:
+    # We only want event type 4, as that is a key press
+    if type == 4 or type == 0x4:
+      print("Key pressed. Code %u, value %u at %d.%d" %
+            (code, value, tv_sec, tv_usec))
+    else if type != 0 or code != 0 or value != 0:
         print("Event type %u, code %u, value %u at %d.%d" % \
             (type, code, value, tv_sec, tv_usec))
     else:
