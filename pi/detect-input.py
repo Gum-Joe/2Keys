7002d3ccb20398f3f04ae96a14f88587503cb441
Kishan <kishansambhi@hotmail.co.uk>
2018-09-04 14:50:15 +0100

    :art: Starr adding detect input for pi via python See https://stackoverflow.com/questions/5060710/format-of-dev-input-event

---
+++ pi/detect-input.py
@@ -0,0 +1,33 @@
+# Code originally from https://stackoverflow.com/questions/5060710/format-of-dev-input-event
+# I don't know how to handle structs, so i'm just leaving as is (i.e. in Python)
+# Would be better if in Java (like rest of project), C++/C (native code), JS (like server)
+# as it means rest of pi side has to be in python 
+import struct
+import time
+import sys
+
+infile_path = "/dev/input/event" + (sys.argv[1] if len(sys.argv) > 1 else "0")
+
+#long int, long int, unsigned short, unsigned short, unsigned int
+FORMAT = 'llHHI'
+EVENT_SIZE = struct.calcsize(FORMAT)
+
+#open file in binary mode
+in_file = open(infile_path, "rb")
+
+event = in_file.read(EVENT_SIZE)
+
+while event:
+    (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
+    print(type)
+
+    if type != 0 or code != 0 or value != 0:
+        print("Event type %u, code %u, value %u at %d.%d" % \
+            (type, code, value, tv_sec, tv_usec))
+    else:
+        # Events with code, type and value == 0 are "separator" events
+        print("===========================================")
+
+    event = in_file.read(EVENT_SIZE)
+
+in_file.close()
