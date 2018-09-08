6f2b76086228604c53e9ebe92e5a26fdf610be07
Kishan <kishansambhi@hotmail.co.uk>
2018-09-08 16:48:07 +0100

    :art: Use normal logger

--- detector/add_keyboard-cli.py
+++ detector/add_keyboard-cli.py
@@ -1,5 +1,9 @@
 from add_keyboard import add_keyboard
 import sys
+from logger import Logger
+logger = Logger("add")
+
+KEYBOARD_NAME = sys.argv[1] if len(sys.argv) > 1 else "keyboard_1"
 
 # IMPORTANT: Don't use non async functions in this.  That includes the logger
 def gen_handler(keyboards):
@@ -8,8 +12,8 @@ def gen_handler(keyboards):
     for keyboard_stop in keyboards:
       print("[DEBUG] ROOT: STOPPING " + keyboard_stop.keyboard)
       await keyboard_stop.stop_watch()
-      print("Writing " + keyboard)
+      logger.info("Writing keyboard " + keyboard + " to " + KEYBOARD_NAME)
       return
   return handler
 
-add_keyboard(sys.argv[1] if len(sys.argv) > 1 else "keyboard_1", gen_handler)
+add_keyboard(KEYBOARD_NAME, gen_handler)
