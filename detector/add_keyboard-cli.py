58878767c7a4b6aaedd3dccd4e3741a307815931
Kishan <kishansambhi@hotmail.co.uk>
2018-09-08 16:36:43 +0100

    :art: Merge funcs

--- detector/add_keyboard-cli.py
+++ detector/add_keyboard-cli.py
@@ -3,15 +3,13 @@ import sys
 
 # IMPORTANT: Don't use non async functions in this.  That includes the logger
 def gen_handler(keyboards, keyboard):
-  async def base_handler():
+  async def handler():
     print("[DEBUG] STOPPING WATCH")
     for keyboard_stop in keyboards:
       print("[DEBUG] ROOT: STOPPING " + keyboard_stop.keyboard)
       await keyboard_stop.stop_watch()
+      print("Writing " + keyboard)
       return
-  async def handler():
-    await base_handler()
-    print("Writing " + keyboard)
   return handler
 
 add_keyboard(sys.argv[1] if len(sys.argv) > 1 else "keyboard_1", gen_handler)
