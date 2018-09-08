cc5d106f2896759f03703b99b89f9e24f3428cdb
Kishan <kishansambhi@hotmail.co.uk>
2018-09-08 14:29:13 +0100

    :art: Watch file, no infinity loops

--- detector/input.js
+++ detector/input.js
@@ -1,14 +1,16 @@
 /**
  * @overview Attempt to get input nodejs
  */
-const { readFile } = require("fs");
+const { readFile, watchFile } = require("fs");
 const struct = require('python-struct');
 
 console.log("READING")
-while (true) {
+
+watchFile("/dev/input/event0", () => {
   readFile("/dev/input/event0", (err, data) => {
     if (err) throw err;
     const read_data = struct.unpack("llHHI", data);
     console.log(read_data);
   });
-}
+});
+
