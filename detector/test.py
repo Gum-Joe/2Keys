910e6f78124420756afa752a7366271dd2e9adf4
Kishan <kishansambhi@hotmail.co.uk>
2018-09-24 20:22:31 +0100

    :ambulance: Correct test script

--- detector/test.py
+++ detector/test.py
@@ -2,5 +2,5 @@ from watch_keyboard import Keyboard
 import yaml
 
 config_file = open("config.yml", "r")
-config = yaml.parse(config_file.read())
+config = yaml.load(config_file.read())
 keyboard = Keyboard(config["keyboard"]["keyboard"])
