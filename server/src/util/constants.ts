a550ae9e9decb1ab47b3bba76255615c4650b085
Kishan <kishansambhi@hotmail.co.uk>
2018-11-05 21:38:29 +0000

    :art: Use mustache for :art: Use mustache for files NEARLY READY FOR ALPHA RELEASE

--- server/src/util/constants.ts
+++ server/src/util/constants.ts
@@ -80,6 +80,9 @@ SendMode Input  ; Recommended for new scripts due to its superior speed and reli
 // Default Service prefix
 export const WINDOWS_DAEMON_PREFIX: string = "2Keys-";
 export const WINDOWS_DAEMON_FILE: string = "daemon.js";
+export const WINDOWS_DAEMON_FILE_VBS: string = "daemon.vbs";
+export const WINDOWS_DAEMON_FILE_JS_TEMPLATE = join(__dirname, `../../assets/${WINDOWS_DAEMON_FILE}`)
+export const WINDOWS_DAEMON_FILE_VBS_TEMPLATE = join(__dirname, `../../assets/${WINDOWS_DAEMON_FILE_VBS}`)
 export const WINDOWS_DAEMON_PID_FILE = "daemon.pid"
 
 // Local project stuff
