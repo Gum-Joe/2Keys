68d8e1b08c4600602d71822437524e9020095a95
Kishan <kishansambhi@hotmail.co.uk>
2018-11-10 11:53:21 +0000

    :art: Sytax fix

--- detector/add_keyboard/sync_keyboard_path.py
+++ detector/add_keyboard/sync_keyboard_path.py
@@ -5,5 +5,5 @@ from util import UPDATE_KEYBOARD_PATH, load_config
 async def update_server_keyboard_path(name, keyboard_path):
   config = await load_config()
   async with aiohttp.ClientSession() as session:
-    async with session.post(f"http://{config["addresses:"]["server"]["ipv4"]}:{config["addresses:"]["server"]["port"]}{UPDATE_KEYBOARD_PATH}", params={ keyboard: name, path: keyboard_path }) as resp:
+    async with session.post("http://" + config["addresses"]["server"]["ipv4"] + ":" + config["addresses"]["server"]["port"] + {UPDATE_KEYBOARD_PATH}, params={ "keyboard": name, "path": keyboard_path }) as resp:
       print(resp)
