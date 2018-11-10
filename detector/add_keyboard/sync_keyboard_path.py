3ddc1cd2bfc526a9d64ef4a69dfef18a8baaa174
Kishan <kishansambhi@hotmail.co.uk>
2018-11-10 12:05:55 +0000

    :art: Fix args

--- detector/add_keyboard/sync_keyboard_path.py
+++ detector/add_keyboard/sync_keyboard_path.py
@@ -1,13 +1,14 @@
 # Sync keyboard path to server
 import aiohttp
-from util import load_config
+from util import load_config, Logger
 from util.constants import UPDATE_KEYBOARD_PATH
 
+logger = Logger("sync")
 async def update_server_keyboard_path(name, keyboard_path):
   config = await load_config()
   try:
     async with aiohttp.ClientSession() as session:
-      async with session.post("http://" + config["addresses"]["server"]["ipv4"] + ":" + config["addresses"]["server"]["port"] + {UPDATE_KEYBOARD_PATH}, params={ "keyboard": name, "path": keyboard_path }) as resp:
+      async with session.post("http://" + config["addresses"]["server"]["ipv4"] + ":" + config["addresses"]["server"]["port"] + {UPDATE_KEYBOARD_PATH}, data={ "keyboard": name, "path": keyboard_path }) as resp:
         print(resp)
   except aiohttp.ClientError as err:
     logger.err(err)
