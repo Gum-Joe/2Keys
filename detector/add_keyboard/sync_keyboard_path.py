f53d5f344893c5af83d282d4c996684fe0f45c0a
Kishan <kishansambhi@hotmail.co.uk>
2018-11-10 12:07:40 +0000

    :art: Generic commi

--- detector/add_keyboard/sync_keyboard_path.py
+++ detector/add_keyboard/sync_keyboard_path.py
@@ -5,10 +5,12 @@ from util.constants import UPDATE_KEYBOARD_PATH
 
 logger = Logger("sync")
 async def update_server_keyboard_path(name, keyboard_path):
+  logger.info("Updating config...")
+  logger.debug("Loading config...")
   config = await load_config()
   try:
     async with aiohttp.ClientSession() as session:
       async with session.post("http://" + config["addresses"]["server"]["ipv4"] + ":" + config["addresses"]["server"]["port"] + {UPDATE_KEYBOARD_PATH}, data={ "keyboard": name, "path": keyboard_path }) as resp:
         print(resp)
-  except aiohttp.ClientError as err:
+  except aiohttp.Error as err:
     logger.err(err)
