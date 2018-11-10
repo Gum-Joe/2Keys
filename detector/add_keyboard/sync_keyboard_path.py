89bcb91e51580138c4332bad0530e0cbb1afdbc9
Kishan <kishansambhi@hotmail.co.uk>
2018-11-10 12:02:46 +0000

    :art: Try err handle

--- detector/add_keyboard/sync_keyboard_path.py
+++ detector/add_keyboard/sync_keyboard_path.py
@@ -5,6 +5,9 @@ from util.constants import UPDATE_KEYBOARD_PATH
 
 async def update_server_keyboard_path(name, keyboard_path):
   config = await load_config()
-  async with aiohttp.ClientSession() as session:
+  try:
+    async with aiohttp.ClientSession() as session:
     async with session.post("http://" + config["addresses"]["server"]["ipv4"] + ":" + config["addresses"]["server"]["port"] + {UPDATE_KEYBOARD_PATH}, params={ "keyboard": name, "path": keyboard_path }) as resp:
       print(resp)
+  except aiohttp.ClientError as err:
+    logger.err(err)
