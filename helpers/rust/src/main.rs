04093c51cb77ca0304b1d2a143963fccf16a84b3
Kishan <kishansambhi@hotmail.co.uk>
2018-11-12 17:31:22 +0000

    :art: Add logger for rust into own file.

--- helpers/rust/src/main.rs
+++ helpers/rust/src/main.rs
@@ -1,20 +1,6 @@
-extern crate colored;
-use colored::*;
-
-struct Logger {
-  name: String
-}
-
-impl Logger {
-    fn info(&self, logline: &str)  {
-        println!("{} {} {}", &self.name.magenta(), "info".green(), logline.to_string());
-    }
-    fn new<Scon>(name: Scon) -> Logger where Scon: Into<String> {
-        Logger { name: name.into() }
-    }
-}
+mod util;
 
 fn main() {
-    let logger = Logger::new("cli");
-    logger.info("Hi");
+    let logger = util::logger::Logger::new("cli");
+    logger.info("2Keys Helper server.");
 }
--- helpers/rust/src/util/logger.rs
+++ helpers/rust/src/util/logger.rs
@@ -3,15 +3,47 @@
  */
 
 extern crate colored;
-use colored::*;
+use self::colored::*;
 
 pub struct Logger {
-  name: str,
+  name: String
 }
 
+impl Logger {
+  /**
+   * Color: Green
+   * Level: Info
+   */
+  pub fn info(&self, logline: &str)  {
+    println!("{} {} {}", &self.name.magenta(), "info".green(), logline.to_string());
+  }
 
-pub impl Logger {
-  fn info(&self, logline: str)  {
-    println!("{} {} {}", &self.name, "info".green(), logline);
+  /**
+   * Color: Yellow
+   * Level: Warning
+   */
+  pub fn warn(&self, logline: &str)  {
+    println!("{} {} {}", &self.name.magenta(), "warn".yellow(), logline.to_string());
+  }
+
+  /**
+   * Color: Cyan
+   * Level: Debug/verbose
+   */
+  pub fn debug(&self, logline: &str)  {
+    println!("{} {} {}", &self.name.magenta(), "debug".cyan(), logline.to_string());
+  }
+
+  /**
+   * Color: Red
+   * Level: Error
+   */
+  pub fn err(&self, logline: &str)  {
+    println!("{} {} {}", &self.name.magenta(), "err".red(), logline.to_string());
+  }
+
+  // Creates a new logger
+  pub fn new<Scon>(name: Scon) -> Logger where Scon: Into<String> {
+    Logger { name: name.into() }
   }
 }
