cd7c47ea29eb9126495a3970f6cdceec1eb4fcd0
Kishan <kishansambhi@hotmail.co.uk>
2018-11-13 20:55:49 +0000

    Use a print

--- detector/add_keyboard/sync_keyboard_path.py
+++ detector/add_keyboard/sync_keyboard_path.py
@@ -17,7 +17,7 @@ async def update_server_keyboard_path(name, keyboard_path):
       config = yaml.load(config_contents) # Parse it into python obj
       logger.debug("Parsed contents: " + str(config))
       try:
-        logger.debug("http://" + config["addresses"]["server"]["ipv4"] +
+        print("http://" + config["addresses"]["server"]["ipv4"] +
                      ":" + config["addresses"]["server"]["port"] + UPDATE_KEYBOARD_PATH)
         async with aiohttp.ClientSession() as session:
           async with session.post("http://" + config["addresses"]["server"]["ipv4"] + ":" + config["addresses"]["server"]["port"] + UPDATE_KEYBOARD_PATH, data={ "keyboard": name, "path": keyboard_path }) as resp:
