fc32017494f29ba270a97814831abb64408bd5c0
Kishan <kishansambhi@hotmail.co.uk>
2020-05-05 13:20:56 +0100

    Fix a test compile error

--- packages/@twokeys/addons/test/non-mocha/loadTest/executor.ts
+++ packages/@twokeys/addons/test/non-mocha/loadTest/executor.ts
@@ -1,6 +1,6 @@
 import TwoKeys from "@twokeys/addons/src/module-interfaces/twokeys";
 
-export const execute = async function (twokeys: TwoKeys, config): Promise<void> {
+export const execute = async function (twokeys: TwoKeys<"executor">, config): Promise<void> {
 	config.testValue = true;
 	config.expect(twokeys).to.have.property("logger");
 	config.expect(twokeys.logger.args.name).to.include("loading-test");
