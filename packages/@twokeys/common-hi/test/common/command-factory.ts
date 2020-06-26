1513bf145d4b1ac414c3352692394c904e80d92a
Kishan <kishansambhi@hotmail.co.uk>
2020-06-26 12:42:23 +0100

    Added tests for command factory. The Base strcuture of commands is now done. Going to do some code tidy up and docs first before completing.

--- packages/@twokeys/common-hi/test/common/command-factory.ts
+++ packages/@twokeys/common-hi/test/common/command-factory.ts
@@ -49,12 +49,48 @@ describe("Command factory tests", () => {
 		});
 		it("should succesfully execute a command, with the right props", (done) => {
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
-			const someCommand = (twokeys: TwoKeys): void => {
+			const someCommand = (twokeys: TwoKeys, config: any): void => {
 				expect(twokeys).to.be.instanceOf(TwoKeys);
 				expect(twokeys.logger.args.name).to.equal("command:commandName");
+				expect(config).to.haveOwnProperty("isHere");
+				expect(config.isHere).to.be.true;
 				done();
 			};
-			expect(() => factory.callCommand(CommandFactory.wrapCommand(someCommand, "commandName"), {})).to.throw(/Property commandName was either undefined/);
+			factory.callCommand(CommandFactory.wrapCommand(someCommand, "commandName"), { isHere: true });
+		});
+	});
+
+	describe("Stateful command execution", () => {
+		const factory = new CommandFactory(TwoKeys);
+		it("should throw an error on a non-command", () => {
+			// eslint-disable-next-line @typescript-eslint/no-empty-function
+			// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+			// @ts-ignore
+			expect(() => factory.createStatefulCommand(class NotACommand {})).to.throw(/Attempted to call a non-command/);
+		});
+		it("should throw an error when not all command info is defined", () => {
+			// eslint-disable-next-line @typescript-eslint/no-empty-function
+			class SomeCommand extends BaseStatefulCommand {
+				public run() {
+					// Empty
+				}
+			}
+			Object.defineProperty(SomeCommand, "commandInfo", { value: {} });
+			expect(() => factory.createStatefulCommand(SomeCommand)).to.throw(/Property commandName was either undefined/);
+		});
+		it("should succesfully execute a command, with the right props", (done) => {
+			// eslint-disable-next-line @typescript-eslint/no-empty-function
+			@CommandFactory.wrapStatefulCommand("someStatefulCommand")
+			class SomeProperCommand extends BaseStatefulCommand {
+				public run(): void {
+					expect(this.twokeys).to.be.instanceOf(TwoKeys);
+					expect(this.twokeys.logger.args.name).to.equal("command:someStatefulCommand");
+					done();
+				}
+			}
+			const theCommand = factory.createStatefulCommand(SomeProperCommand);
+			expect(theCommand).to.be.instanceOf(SomeProperCommand);
+			theCommand.run();
 		});
 	});
 });
