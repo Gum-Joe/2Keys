4497e3f2bde144d8bc0030ddf2caddbd5b9529c8
Kishan <kishansambhi@hotmail.co.uk>
2020-07-05 19:37:30 +0100

    Some changes so we can run all tests at once Tests: 116 Time to run: ~2 minutes

--- .mocharc.js
+++ .mocharc.js
@@ -4,6 +4,7 @@ module.exports = {
 	"r": "ts-node/register",
 	"recursive": true,
 	"spec": ["packages/@twokeys/*/test/*.ts", " packages/@twokeys/*/test/**/*.ts", "executors/@twokeys/*/test/*.ts"],
+	"exclude": ["packages/@twokeys/server/test/**/*.ts"],
 	"unhandled-rejections": "strict",
 	"timeout": 50000,
 }
