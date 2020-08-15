fd5a86559154704dc414db5372d0e8cabbb562d6
Madeline H <james.s.hart@outlook.com>
2020-08-15 21:59:34 +0100

    change commandsList to export Map

--- packages/@twokeys/common-hi/src/commandsList.ts
+++ packages/@twokeys/common-hi/src/commandsList.ts
@@ -1,16 +1,15 @@
 /* eslint-disable */
-	// FILE COMPILED BY scripts/commands-list.ts
-	// DO NOT MODIFY
+// FILE COMPILED BY scripts/commands-list.ts
+// DO NOT MODIFY
 
-	// Imports
-	import command0 from "./setup/commands/daemon"
+// Imports
+import command0 from "./setup/commands/daemon"
 import command1 from "./setup/commands/oobe"
 import command2 from "./setup/commands/project"
 
-	// Map
-	export = {
-	"generateProjectDaemon": command0,
-"oobe": command1,
-"createProject": command2,
-	}
-	
+// Map
+export = new Map([
+	["generateProjectDaemon", command0],
+	["oobe", command1],
+	["createProject", command2],
+]);
