ba9eda38e0f2e3b7bf5c8f7ff7439781d0a092be
Kishan <kishansambhi@hotmail.co.uk>
2020-08-20 19:12:02 +0100

    Add IPv4 change test

--- packages/@twokeys/common-hi/vm-test/test.ts
+++ packages/@twokeys/common-hi/vm-test/test.ts
@@ -1,5 +1,53 @@
-import { networkInterfaces } from "os";
+import { networkInterfaces, platform } from "os";
+import setStaticIPv4Address from "../src/setup/util/setIPv4";
+import { TwoKeys } from "../test/test-util/command-factory";
+import { expect } from "chai";
 
-it("should do stuff", () => {
-	console.log(networkInterfaces());
-});
+/** Change IPv4 of an env var or default one for GitHub Actions */
+export const NETWORK_INTERFACE = process.env.TWOKEYS_OOBE_NET_TEST_INTERFACE || "vEthernet (nat)";
+
+describe("IPv4 setting tests", function () {
+	if (platform() !== "win32") return;
+
+	it("should successfully change the IP address", async function () {
+		// eslint-disable-next-line no-prototype-builtins
+		if (!networkInterfaces().hasOwnProperty(NETWORK_INTERFACE)) {
+			this.skip();
+		}
+
+		// get new interface
+		function addOneToIPv4(ip: string): string {
+			const numbers = ip.split(".").map(ipSection => parseInt(ipSection, 10));
+			numbers[numbers.length - 1] = numbers[numbers.length - 1] + 1;
+			if (numbers[numbers.length - 1] > 255) {
+				numbers[numbers.length - 1] = 0;
+			}
+
+			return numbers.map(ipNo => ipNo.toFixed(0)).join(".");
+		}
+
+		const oldIp = networkInterfaces()[NETWORK_INTERFACE].filter(value => value.family === "IPv4")[0].address;
+		const newIp = addOneToIPv4(oldIp);
+
+		// DEW IT
+		await setStaticIPv4Address(new TwoKeys({
+			commandName: "ipv4"
+		}, {}), {
+			networkAdapter: NETWORK_INTERFACE,
+			ipv4: newIp,
+		});
+
+		expect(networkInterfaces()[NETWORK_INTERFACE].filter(value => value.family === "IPv4")[0].address).to.equal(newIp);
+
+		// Reset
+		await setStaticIPv4Address(new TwoKeys({
+			commandName: "ipv4"
+		}, {}), {
+			networkAdapter: NETWORK_INTERFACE,
+			ipv4: oldIp,
+		});
+
+		expect(networkInterfaces()[NETWORK_INTERFACE].filter(value => value.family === "IPv4")[0].address).to.equal(oldIp);
+
+	});
+})
