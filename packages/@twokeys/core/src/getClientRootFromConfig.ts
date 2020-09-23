4ccb15a083e78f2be4775b06f1e45687ff187d77
Kishan <kishansambhi@hotmail.co.uk>
2020-09-23 21:46:29 +0100

    Fix compile errors

--- packages/@twokeys/core/src/getClientRootFromConfig.ts
+++ packages/@twokeys/core/src/getClientRootFromConfig.ts
@@ -1,6 +1,5 @@
-import { TWOKEYS_CLIENT_STORAGE_ROOT } from "@twokeys/core/lib/constants";
+import { TWOKEYS_CLIENT_STORAGE_ROOT } from "./constants";
 import { join } from "path";
-import { ClientConfig } from "@twokeys/core/lib/interfaces";
 
 export default function getClientRootFromConfig(config: { id: string }): string {
 	return join(TWOKEYS_CLIENT_STORAGE_ROOT, config.id);
