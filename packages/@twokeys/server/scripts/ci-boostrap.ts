6042ea3e239a064294b99120a22b93a2b0e7d18e
Kishan <kishansambhi@hotmail.co.uk>
2020-10-05 21:52:08 +0100

    Update package.json

--- packages/@twokeys/server/scripts/ci-boostrap.ts
+++ packages/@twokeys/server/scripts/ci-boostrap.ts
@@ -3,7 +3,7 @@
  * @packageDocumentation
  */
 import { Logger } from "@twokeys/core";
-import { symlinkSync } from "fs";
+import { symlinkSync, writeFileSync } from "fs";
 import mkdirp from "mkdirp";
 import { basename, join } from "path";
 import { MOCK_REGISTRY } from "../test/boostrapper";
@@ -23,4 +23,19 @@ logger.warn("Please ensure yarn has been ran before.");
 logger.info(`Linking ${EXECUTOR_AHK} -> ${join(TARGET_ROOT, basename(EXECUTOR_AHK))}`);
 symlinkSync(EXECUTOR_AHK, join(TARGET_ROOT, basename(EXECUTOR_AHK)));
 logger.info(`Linking ${DETECTOR_DESKTOP} -> ${join(TARGET_ROOT, basename(DETECTOR_DESKTOP))}`);
-symlinkSync(DETECTOR_DESKTOP, join(TARGET_ROOT, basename(DETECTOR_DESKTOP)));
+symlinkSync(DETECTOR_DESKTOP, join(TARGET_ROOT, "detector-desktop"));
+
+logger.info("Adding to package.json");
+
+const registryJSONPath = join(MOCK_REGISTRY, "package.json");
+const registryJSON = require(registryJSONPath);
+const executorAHK = require(join(EXECUTOR_AHK, "package.json"));
+const detectorDesktop = require(join(DETECTOR_DESKTOP, "package.json"));
+registryJSON.dependencies = {
+	...registryJSON.dependencies
+};
+
+registryJSON.dependencies[executorAHK.name] = executorAHK.version;
+registryJSON.dependencies[detectorDesktop.name] = detectorDesktop.version;
+
+writeFileSync(registryJSONPath, JSON.stringify(registryJSON));
