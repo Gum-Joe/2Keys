75b81da81c07bd6b714f3ff82005db2e28439f74
Kishan <kishansambhi@hotmail.co.uk>
2020-10-17 21:12:18 +0100

    Copy server stuff to legacy server This way we can remove unused code from the main server and still keep what we have already written E.g. CLI and OOBE code.

---
+++ packages/@twokeys/legacy-server/src/util/logger.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright 2020 Kishan Sambhi
+ *
+ * This file is part of 2Keys.
+ *
+ * 2Keys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 2Keys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 2Keys.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/**
+ * Logging module for 2Keys, from https://github.com/Gum-Joe/tara
+ * @packageDocumentation
+ * @module logger
+ */
+import { Logger } from "@twokeys/core";
+
+/**
+ * Previously there was a logger here
+ * But that has now been move to the package @twokeys/core
+ * So we just use that here
+ */
+export default Logger;
