add63d4180198b72c8381ffc76c390a7d59ab660
Kishan <kishansambhi@hotmail.co.uk>
2020-12-17 21:23:15 +0000

    Add direcotyr creation

--- detectors/detector-pi/detector/twokeys/provision/provision.py
+++ detectors/detector-pi/detector/twokeys/provision/provision.py
@@ -1,18 +1,21 @@
 
+from os import makedirs, mkdir
+import os
 from ..util.constants import SCHEMA_PROVISIONING
 from twokeys_utils import Logger
 from ..util.config import load_config, load_config_from_file
 from jsonschema import validate
 from jsonschema.exceptions import SchemaError, ValidationError
 import json
-from typing import Any
+from typing import Any, Union, Dict
 
 logger = Logger("provision")
 
 class FatalProvisioningException(Exception):
 	"""Used to tell caller (CLI) there was an error and to quit with non-exit code"""
 
-def validateConfig(configFile: str | False) -> dict[str, Any]:
+
+def validateConfig(configFile: Union[str, None]) -> Dict[str, Any]:
 	"""Ensures the config provided to us exists and is valid. Returns the parsed config."""
 	logger.info("Validating config...")
 	logger.debug(configFile)
@@ -26,18 +29,24 @@ def validateConfig(configFile: str | False) -> dict[str, Any]:
 		logger.err("Error reading config!")
 		logger.err(err)
 		raise FatalProvisioningException("Could not read provided config")
+
+	# Config now loaded
 	try:
 		logger.debug("Reading schema for validation")
 		loadedSchemaRAW = open(SCHEMA_PROVISIONING, "r")
 		parsedSchema = json.load(loadedSchemaRAW)
+
 		# Validate logger.debug("Validating...")
 		if config is None:
 			logger.err("No config given when the file was loaded!")
 			raise FatalProvisioningException("No config given when it was loaded!")
+
 		validate(config, parsedSchema)
+
 		# And if that's fine:
 		logger.debug("Schema valid!")
 		return config
+	# Handle exception
 	except IOError as err:
 		logger.err("Internal error reading schema to validate user-provided config!")
 		logger.err("This may be a problem with your 2Keys install.  Please reinstall 2Keys, or file an issue.")
@@ -74,7 +83,17 @@ def provision(**kargs):
   ```
   """
 	logger.info("Provisioning a new client")
-	config = validateConfig(kargs)
+	config = validateConfig(kargs["file"])
 	logger.info("Schema validation succesful. Starting install...")
-	
 
+	# STEP 1: Create directors
+	logger.info("============================")
+	logger.info("	BEGIN PROVISION")
+	logger.info("============================")
+	logger.info("Creating directories...")
+	if not os.path.exists(config["client"]["roots"]["config"]):
+		makedirs(config["client"]["roots"]["config"])  # For configs
+		logger.info("Created directory " + config["client"]["roots"]["config"] + " to store configs in.", )
+
+	if not os.path.exists(config["client"]["roots"]["projects"]):  # For projects storage
+		logger.info("Created directory " + config["client"]["roots"]["projects"] + " to store configs in.", )
