3edca99211d6ddc5011aee6cbd16243f524990e6
Kishan <kishansambhi@hotmail.co.uk>
2020-12-20 22:41:55 +0000

    Add a setup.py to twokeys_utils This way we can install it.

--- detectors/common/python/setup.py
+++ detectors/common/python/setup.py
@@ -2,8 +2,6 @@
 # https://python-packaging.readthedocs.io/en/latest/everything.html
 from setuptools import setup, find_packages
 
-from .twokeys.util.constants import VERSION
-
 def readme():
     with open("./README.md") as f:
         return f.read()
@@ -17,16 +15,16 @@ def get_required():
     
     return packages
 
-setup(name="2Keys",
-      version=VERSION,
-      description="A easy to setup second keyboard, designed for everyone. ",
+setup(name="2Keys-utils",
+      version="0.4.0",
+      description="Common Python Code for 2Keys detectors written in Python",
       long_description=readme(),
       url="https://github.com/Gum-Joe/2Keys",
       author="Gum-Joe",
       author_email="kishansambhi@hotmail.co.uk",
       keywords="hid ahk autohotkey macros 2cdkeyboard keyboards",
       license="GPLv3",
-      packages=find_packages(),
+      packages=["twokeys_utils"], # Might include tests!
       install_requires=get_required(),
       classifiers=[
           "Development Status :: 2 - Pre-Alpha",
@@ -35,13 +33,5 @@ setup(name="2Keys",
           "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
       ],
       include_package_data=True,
-      entry_points={
-        "console_scripts": ["2Keys = twokeys.cli:cli"]
-      },
       zip_safe=False,
-
-      # For bdist
-      package_data={
-        "package": ["assets/*", "assets/schemas/*"]
-      }
 )
